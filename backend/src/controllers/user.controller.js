295f8b2899d639efee265af0c54496564e18fc85
Suyog Aldar <suyogaldar07@gmail.com>
2026-06-04 11:38:14 +0530

    Create user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,52 +1,30 @@
-import { User } from "../models/user.model.js";
+import User from "../models/user.model.js";
 import jsonwebtoken from "jsonwebtoken";
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
+import responseHandler from "../handlers/response.handler.js";
 
 const signUp = async (req, res) => {
     try {
-        const { userName, fullName, password, confirmPassword } = req.body
+        const { userName, displayName, password, confirmPassword } = req.body
 
-        if (!userName || !fullName || !password || !confirmPassword) {
-            throw new ApiError(401, "All fields are required")
-        }
+        if (!userName || !displayName || !password || !confirmPassword)
+            return responseHandler.badrequest(res, "All fields are required")
 
-        if (!(password === confirmPassword)) {
-            throw new ApiError(401, "Password and ConfirmPassword should be same")
-        }
+        if (!(password === confirmPassword))
+            return responseHandler.badrequest(res, "Password and Confirm password should be same")
 
         const checkUser = await User.findOne(userName)
 
         if (checkUser) {
-            throw new ApiError(401, "User already exist")
+            return responseHandler.badrequest(res, "username already exsist")
         }
 
-        let avaterImageLocalPath;
+        const user = new User()
 
-        if (req.files && Array.isArray(req.files.avaterImage) && req.files.avaterImage.length > 0) {
-            avaterImageLocalPath = req.files.avaterImage[0].path
-        }
+        user.userName = userName
+        user.displayName = displayName
+        user.setPassword(password)
 
-        const avaterImage = await uploadOnCloudinary(avaterImageLocalPath, "avater")
-
-        const user = await User.create({
-            userName,
-            fullName,
-            avaterImage: {
-                url: avaterImage?.secure_url,
-                public_id: avaterImage?.public_id
-            } || {
-                url: "https://res.cloudinary.com/spidyboy-1707/image/upload/v1777763508/avaterImage.png",
-                public_id: avaterImage
-            },
-            password
-        })
-
-        const createdUser = await User.findById(user.id)
-
-        if (!createdUser) {
-            throw new ApiError(500, "somthing went wrong while creating user")
-        }
+        await user.save
 
         const token = jsonwebtoken.sign(
             { data: user.id },
@@ -54,19 +32,87 @@ const signUp = async (req, res) => {
             { expiresIn: "24h" }
         )
 
-        return res
-            .status(200)
-            .json(
-                new ApiResponse(
-                    200,
-                    {
-                        token,
-                        user: createdUser._doc,
-                        id: user.id
-                    }
-                )
-            )
-    } catch (error) {
-        throw new ApiError(500, "Somthing went Wrong")
+        responseHandler.created(res, {
+            token,
+            ...user._doc,
+            id: user.id
+        })
+
+    } catch {
+        responseHandler.error(res)
     }
+}
+
+const signIn = async (req, res) => {
+    try {
+        const { userName, password } = req.body
+
+        const user = await User.findOne({ userName }).select("userName password salt id displayName")
+
+        if (!user)
+            return responseHandler.badrequest(res, "username dosen't exsist")
+
+        if (!user.validPassword(password))
+            return responseHandler.badrequest(res, "Password incorrect")
+
+        const token = jsonwebtoken.sign(
+            { data: user.id },
+            process.env.TOKEN_SECRET,
+            { expiresIn: "24h" }
+        )
+
+        user.password = undefined
+        user.salt = undefined
+
+        responseHandler.created(res, {
+            token,
+            ...user._doc,
+            id: user.id
+        })
+
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
+const updatePassword = async (req, res) => {
+    try {
+        const {password, newPassword} = req.body
+
+        const user = await User.findById(req.user.id).select("password id salt")
+
+        if (!user) 
+            return responseHandler.unauthorize(res)
+
+        if (!user.validPassword(password))
+            return responseHandler.badrequest(res, "Passsword incorrect")
+
+        user.setPassword(newPassword)
+
+        await user.save
+
+        responseHandler.ok(res)
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
+const getInfo = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id)
+
+        if (!user)
+            return responseHandler.notFound(res)
+
+        responseHandler.ok(res, user)
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
+export default {
+    signUp,
+    signIn,
+    updatePassword,
+    getInfo
 }
