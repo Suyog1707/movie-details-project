eb562bcde1bd40528c5fa8d46aca9e22368a3149
Suyog Aldar <suyogaldar07@gmail.com>
2026-06-04 13:12:58 +0530

    create favorite controller

---
+++ backend/src/controllers/favorite.controller.js
@@ -0,0 +1,61 @@
+import responseHandler from "../handlers/response.handler.js";
+import Favorite from "../models/favorite.model.js"
+
+const addFavorite = async (req, res) => {
+    try {
+        const isFavorite = await Favorite.findOne({
+            user: req.user.id,
+            mediaId: req.body.mediaId
+        })
+
+        if (isFavorite)
+            return responseHandler.ok(res, isFavorite)
+
+        const favorite = new Favorite({
+            ...req.body,
+            user: req.user.id
+        })
+
+        await favorite.save
+
+        responseHandler.created(res, favorite)
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
+const removeFavorite = async (req, res) => {
+    try {
+        const { favoriteId } = req.params
+
+        const favorite = await Favorite.findOne({
+            user: req.user.id,
+            _id: favoriteId
+        })
+
+        if (!favorite)
+            return responseHandler.notFound(res)
+
+        await favorite.remove()
+
+        return responseHandler.ok(res)
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
+const getFavoritesOfUser = async (req, res) => {
+    try {
+        const favorite = await Favorite.find({ user: req.user.id }).sort("-createdAt")
+
+        return responseHandler.ok(res, favorite)
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
+export default {
+    addFavorite,
+    removeFavorite,
+    getFavoritesOfUser
+}
