38077e1ea7f837ea65d04aee50b546448e740247
Suyog Aldar <suyogaldar07@gmail.com>
2026-06-06 15:02:31 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(
-    "/",
+    "api/v1",
     (req,res) => {
         res.send("Server is Running Properly")
     })
